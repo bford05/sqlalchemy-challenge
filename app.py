3cc36294a96266d5c907728a07ec6c1a99cc0121
Brian <cbd.belapur.15@gmail.com>
2020-04-12 14:15:56 -0400

    added routes for start and end dates to py file. minor updates to analysis

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
-
 from flask import Flask, jsonify
 
 #Setup Database
@@ -31,14 +29,18 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     print("List all available api routes")
-    return (f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start<br/>"
-        f"/api/v1.0/start/end<br>"
+    return (f"Available Routes:<br/><br/>"
+        f"Precipitation Data for One Year<br/>"
+        f"/api/v1.0/precipitation<br/><br/>"
+        f"List of Stations:<br/>"
+        f"/api/v1.0/stations<br/><br/>"
+        f"List of Temperatures for 2017:<br/>"
+        f"/api/v1.0/tobs<br/><br/>"
+        f"Min, Max, and Avg Temperatures from start date yyyy-mm-dd:<br/>"
+        f"/api/v1.0/<start><br/><br/>"
+        f"Min, Max, and Avg Temperatures from start to end date: yyyy-mm-dd/yyyy-mm-dd<br/>"
+        f"/api/v1.0/<start>/<end><br/>"
     )
-
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
@@ -52,16 +54,17 @@ def precipitation():
     order_by(Measurement.date).all()
 
 
-    #Create dictionary and append date and precipitation values from query
-    #Return json format of precipitation data
+    #Retrieve date and precipitation values from query results
+    #Create dictionary and append values to empty list
     
     precipitation_data = []
     for date, prcp in prcp_data:
         precipitation_dict = {}
         precipitation_dict["date"] = date
-        precipitation_dict["prcp"] = prcp
+        precipitation_dict["precipitation"] = prcp
         precipitation_data.append(precipitation_dict)
 
+    #Render precipitation data in json format
     return jsonify(precipitation_data)
 
 @app.route("/api/v1.0/stations")
@@ -75,13 +78,14 @@ def stations():
     group_by(Measurement.station).\
     order_by(Measurement.station).all()
 
-    #Return list of stations in json format
+    #Retrieve stations data from query results, create dictionary, and append to empty list
     stations_list = []
     for station in stations:
         station_dict = {}
         station_dict["station"] = station
         stations_list.append(station_dict)
 
+    #Render station data in json format thru app
     return jsonify(stations_list)
 
 @app.route("/api/v1.0/tobs")
@@ -97,15 +101,62 @@ def temperatures():
     filter(Measurement.date <= '2017-12-31').\
     group_by(Measurement.date).all()
 
-    #Return temperature observations for the most active station for the last year in json format
+    #Retrieve date and temp values from query results, create dictionary, and append to empty list
     temps_list = []
-    for date, tobs in stations_temp:
+    for date, temperature in stations_temp:
         temp_dict = {}
         temp_dict["date"] = date
-        temp_dict["tobs"] = tobs
+        temp_dict["temperature"] = temperature
         temps_list.append(temp_dict)
 
+    #Render temp values in json format thru app
     return jsonify(temps_list)
+
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+
+    #Create session to link database to Python
+    session = Session(engine)
+
+    #Design query to return min, max, and avg temperatures for any date entered
+    temp = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+    filter(Measurement.date >= start).all()
+
+    #Retrieve min, max, and avg temp results from query, create dictionary, and append to empty list
+    temp_values_date = []
+    for min_temp, max_temp, avg_temp in temp:
+        temp_value_dict = {}
+        temp_value_dict["min temperature"] = min_temp
+        temp_value_dict["max temperature"] = max_temp
+        temp_value_dict["avg temperature"] = avg_temp
+        temp_values_date.append(temp_value_dict)
+    
+    #Render temp values in json format thru app
+    return jsonify(temp_values_date)
+
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_date(start, end):
+
+    #Create session to link database to Python
+    session = Session(engine)
+
+    #Design query to return min, max, and avg temperatures for all applicable start and end date range entered
+    temp_date_range = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+    filter(Measurement.date >= start, Measurement.date <= end).all()
+
+    #Retrieve min, max, and avg temp values from query results, create dictionary, and append to empty list
+    temp_values_date_range = []
+    for min_temp, max_temp, avg_temp in temp_date_range:
+        temp_value_range_dict = {}
+        temp_value_range_dict["min temperature"] = min_temp
+        temp_value_range_dict["max temperature"] = max_temp
+        temp_value_range_dict["avg temperature"] = avg_temp
+        temp_values_date_range.append(temp_value_range_dict)
+    
+    #Render temp values in json format thru app
+    return jsonify(temp_values_date_range)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
